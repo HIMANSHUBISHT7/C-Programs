70220f0c710634b21f6eb3815dfc21d96daf94d4
HIMANSHUBISHT7 <137715136+HIMANSHUBISHT7@users.noreply.github.com>
2023-07-19 17:14:03 +0530

    Add files via upload

---
+++ aroc.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+// Area of circle
+int main() {
+    float radius;
+    
+    printf("Enter radius : ");
+    scanf("%f",&radius);
+
+    printf("area of circle is : %.2f",(radius*radius)*22/7);
+
+    return 0;  
+} 
