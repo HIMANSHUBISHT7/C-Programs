6a44cabf47f7d981c731f257bf0a44f673f41123
HIMANSHUBISHT7 <137715136+HIMANSHUBISHT7@users.noreply.github.com>
2024-08-16 20:12:08 +0530

    aroc.c

--- aroc.c
+++ aroc.c
@@ -2,11 +2,8 @@
 // Area of circle
 int main() {
     float radius;
-    
     printf("Enter radius : ");
     scanf("%f",&radius);
-
     printf("area of circle is : %.2f",(radius*radius)*22/7);
-
     return 0;  
 } 
