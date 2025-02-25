18a6d1d1a3a67d9e891ef33d4e38de9fbc0b00b0
HIMANSHUBISHT7 <137715136+HIMANSHUBISHT7@users.noreply.github.com>
2025-02-25 20:09:06 +0530

    Add files via upload

---
+++ aropr.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+// Area of a parallelograme
+int main(){
+
+    int base;
+    int height;
+
+    printf("enter base : ");
+    scanf("%d",&base);
+    printf("enter height : ");
+    scanf("%d",&height);
+
+    printf("area of parallelograme : %d",base*height);
+
+    return 0;
+}
